276f48bb3b0b1405a6db2d932df3927e8cdba83a
shiks <shiks@gmail.com>
2022-11-03 12:41:18 +0300

    amends

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 // jQuery();
-document.getElementById("btn").innerHTML="";
-jQuery(selector).userAction();
+//document.getElementById("btn").innerHTML="";
+// jQuery("#btn").click(function(){
+//     alert("doing something")
+// });
 
-function doSomething(){
-    alert("doing something");
-}
+
+// //function doSomething(){
+//    $(document).ready(function())
+//    $(selector).userAction(callback);
+
+//$("#btn").click(function(){
+   // alert("doing something")
+
+    $(document).ready(function(){
+        $(window).resize(function() {
+            $(".my-btns").css("display", "block");
+        });
+        $("#btn-one").click(function() {
+            alert("Do something");
+        });
+    
+        $("#p-one").click(function() {
+            $(this).text("updated")//css
+            // document.getElementById("p-one").innerHTML = "updated";
+        });
+    
+        (".p-two").click(function() {
+            alert("Do something for p 2");
+        });
+        $("#image").hover(function(){
+           $(this).mouseenter(function(){
